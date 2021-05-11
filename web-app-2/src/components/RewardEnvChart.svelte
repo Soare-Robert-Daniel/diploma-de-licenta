ec3c93c04e69a54f20ead39fe0ce169dab4a3c65
S. Robert <soarerobert17@gmail.com>
2021-05-11 15:39:59 +0300

    Add echart package

---
+++ web-app-2/src/components/RewardEnvChart.svelte
@@ -0,0 +1,30 @@
+<script>
+    import { onMount } from "svelte";
+    import * as echarts from "echarts/core";
+    import {
+        TitleComponent,
+        TooltipComponent,
+        GridComponent,
+    } from "echarts/components";
+    import { BarChart } from "echarts/charts";
+    import { CanvasRenderer } from "echarts/renderers";
+
+    echarts.use([
+        TitleComponent,
+        TooltipComponent,
+        GridComponent,
+        BarChart,
+        CanvasRenderer,
+    ]);
+
+    onMount(() => {});
+
+    let chartLocation;
+</script>
+
+<div class="container">
+    <div bind:this={chartLocation} id="main" />
+</div>
+
+<style lang="scss">
+</style>
